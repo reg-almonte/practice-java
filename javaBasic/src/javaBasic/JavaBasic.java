8e3443b66dc99da57c062dcdc3c09c17712d4ba8
KM_V4_CI <jr@alink-group.co.jp>
2021-02-03 14:36:36 +0900

    * JavaBasic class Implemented literals()

--- javaBasic/src/javaBasic/JavaBasic.java
+++ javaBasic/src/javaBasic/JavaBasic.java
@@ -11,7 +11,7 @@ public class JavaBasic {
 		System.out.println("1. Primitive Data Type:");
 		System.out.println("1.1. List of Primitive Data Types:");
 		primitiveDataTypes();
-		System.out.println("\n3. Literals:");
+		System.out.println("\n1.2. Literals:");
 		literals();
 
 	}
@@ -197,59 +197,7 @@ public class JavaBasic {
 	    System.out.println("\"abc\".substring(2,3) = " + c);
 	    System.out.println("\"cde\".substring(1,2) = " + d);
 	}
-		
-	static void literals() {
-		/* * Default Values
-		 * It's not always necessary to assign a value when a field is declared. Fields that are declared 
-		 * but not initialized will be set to a reasonable default by the compiler. Generally speaking, 
-		 * this default will be zero or null, depending on the data type. Relying on such default values, 
-		 * however, is generally considered bad programming style.*/
-		
-		/* 1.2. Literals
-		 * You may have noticed that the "new" keyword isn't used when initializing a variable of a primitive 
-		 * type. Primitive types are special data types built into the language; they are not objects created 
-		 * from a class. A literal is the source code representation of a fixed value; literals are represented 
-		 * directly in your code without requiring computation. As shown below, it's possible to assign a literal 
-		 * to a variable of a primitive type:*/
-		
-		boolean result = true;
-		char capitalC = 'C';
-		byte b = 100;
-		short s = 10000;
-		int i = 100000;
-		
-		System.out.println("\nSample Literals:");
-		System.out.println("1. boolean result = true: " + result);
-		System.out.println("2. char capitalC = 'C': " + capitalC);
-		System.out.println("3. byte b = 100: " + b);
-		System.out.println("4. short s = 10000: " + s);
-		System.out.println("5. int i = 100000: " + i);
-		
-		/* 1.2.1 Integer Literals
-		 * An integer literal is of type long if it ends with the letter L or l; otherwise it is of type int. 
-		 * It is recommended that you use the upper case letter L because the lower case letter l is hard to 
-		 * distinguish from the digit 1.
-		 * Values of the integral types byte, short, int, and long can be created from int literals. Values of 
-		 * type long that exceed the range of int can be created from long literals. Integer literals can be 
-		 * expressed by these number systems:
-		 * 
-		 * 	- Decimal: Base 10, whose digits consists of the numbers 0 through 9; this is the number system you use every day
-		 * 	- Hexadecimal: Base 16, whose digits consist of the numbers 0 through 9 and the letters A through F
-		 * 	- Binary: Base 2, whose digits consists of the numbers 0 and 1 (you can create binary literals in Java SE 7 and later)
-		 * 
-		 * For general-purpose programming, the decimal system is likely to be the only number system you'll ever use. 
-		 * However, if you need to use another number system, the following example shows the correct syntax. 
-		 * The prefix 0x indicates hexadecimal and 0b indicates binary: */
-		  
-		// The number 26, in decimal
-		int decVal = 26;
-		//  The number 26, in hexadecimal
-		int hexVal = 0x1a;
-		// The number 26, in binary
-		int binVal = 0b11010;
-		
-	}
-		
+	
 	static void floatAndDoubleExperiments() {
 		System.out.println("\n ** Double and Float variable experiments:\n");
 		// An example of overflow:
@@ -288,4 +236,173 @@ public class JavaBasic {
         System.out.println((int)d + " " + (int)(-d));
 	}
 
+	static void literals() {
+		/* * Default Values
+		 * It's not always necessary to assign a value when a field is declared. Fields that are declared 
+		 * but not initialized will be set to a reasonable default by the compiler. Generally speaking, 
+		 * this default will be zero or null, depending on the data type. Relying on such default values, 
+		 * however, is generally considered bad programming style.*/
+		
+		/* 1.2. Literals
+		 * You may have noticed that the "new" keyword isn't used when initializing a variable of a primitive 
+		 * type. Primitive types are special data types built into the language; they are not objects created 
+		 * from a class. A literal is the source code representation of a fixed value; literals are represented 
+		 * directly in your code without requiring computation. As shown below, it's possible to assign a literal 
+		 * to a variable of a primitive type:*/
+		
+		boolean result = true;
+		char capitalC = 'C';
+		byte b = 100;
+		short s = 10000;
+		int i = 100000;
+		
+		System.out.println("Example of Literals:");
+		System.out.println("1. boolean result = true: " + result);
+		System.out.println("2. char capitalC = 'C': " + capitalC);
+		System.out.println("3. byte b = 100: " + b);
+		System.out.println("4. short s = 10000: " + s);
+		System.out.println("5. int i = 100000: " + i);
+		
+		/* 1.2.1 Integer Literals
+		 * An integer literal is of type long if it ends with the letter L or l; otherwise it is of type int. 
+		 * It is recommended that you use the upper case letter L because the lower case letter l is hard to 
+		 * distinguish from the digit 1.
+		 * Values of the integral types byte, short, int, and long can be created from int literals. Values of 
+		 * type long that exceed the range of int can be created from long literals. Integer literals can be 
+		 * expressed by these number systems:
+		 * 
+		 * 	- Decimal: Base 10, whose digits consists of the numbers 0 through 9; this is the number system you use every day
+		 * 	- Hexadecimal: Base 16, whose digits consist of the numbers 0 through 9 and the letters A through F
+		 * 	- Binary: Base 2, whose digits consists of the numbers 0 and 1 (you can create binary literals in Java SE 7 and later)
+		 * 
+		 * For general-purpose programming, the decimal system is likely to be the only number system you'll ever use. 
+		 * However, if you need to use another number system, the following example shows the correct syntax. 
+		 * The prefix 0x indicates hexadecimal and 0b indicates binary: */
+		  
+		// The number 26, in decimal
+		int decVal = 26;
+		//  The number 26, in hexadecimal
+		int hexVal = 0x1a;
+		// The number 26, in binary
+		int binVal = 0b11010;
+		System.out.println("\n1.2.1 Example of Integer Literals:");
+		System.out.println("1. int decVal = 26: " + decVal);
+		System.out.println("2. int hexVal = 0x1a: " + hexVal);
+		System.out.println("3. int binVal = 0b11010: " + binVal);
+		
+		/* 1.2.2 Floating-Point Literals
+		 * A floating-point literal is of type float if it ends with the letter F or f; otherwise its type is double and 
+		 * it can optionally end with the letter D or d. The floating point types (float and double) can also be expressed 
+		 * using E or e (for scientific notation), F or f (32-bit float literal) and D or d (64-bit double literal; this is 
+		 * the default and by convention is omitted). */
+		double d1 = 123.4;
+		// same value as d1, but in scientific notation
+		double d2 = 1.234e2;
+		float f1  = 123.4f;
+		System.out.println("\n1.2.2 Example of Floating-Point Literals:");
+		System.out.println("1. double d1 = 123.4: " + d1);
+		System.out.println("2. double d2 = 1.234e2: " + d2);
+		System.out.println("3. float f1  = 123.4f: " + f1);
+		
+		/* 1.2.3 Character and String Literals
+		 * Literals of types char and String may contain any Unicode (UTF-16) characters. If your editor and file system allow 
+		 * it, you can use such characters directly in your code. If not, you can use a "Unicode escape" such as '\u0108' 
+		 * (capital C with circumflex), or "S\u00ED Se\u00F1or" (Sí Señor in Spanish). Always use 'single quotes' for char literals 
+		 * and "double quotes" for String literals. Unicode escape sequences may be used elsewhere in a program (such as in field 
+		 * names, for example), not just in char or String literals.
+		 * 
+		 * The Java programming language also supports a few special escape sequences for char and String literals: \b (backspace), 
+		 * \t (tab), \n (line feed), \f (form feed), \r (carriage return), \" (double quote), \' (single quote), and \\ (backslash).
+		 * 
+		 * There's also a special null literal that can be used as a value for any reference type. null may be assigned to any variable, 
+		 * except variables of primitive types. There's little you can do with a null value beyond testing for its presence. Therefore, 
+		 * null is often used in programs as a marker to indicate that some object is unavailable.
+		 * 
+		 * Finally, there's also a special kind of literal called a class literal, formed by taking a type name and appending ".class"; 
+		 * for example, String.class. This refers to the object (of type Class) that represents the type itself.
+		 * */
+		char c1 = '\u0108';
+		String s1 = "S\u00ED Se\u00F1or";
+		String s2 = String.class.getName();
+		
+		System.out.println("\n1.2.3 Example of Character and String Literals:");
+		System.out.println("1. char c1 = '\\u0108': " + c1);
+		System.out.println("2. String s1 = \"S\\u00ED Se\\u00F1or\": " + s1);
+		System.out.println("3. String s2 = String.class.getName(): " + s2);
+		
+		/* 1.2.4 Using Underscore Characters in Numeric Literals
+		 * In Java SE 7 and later, any number of underscore characters (_) can appear anywhere between digits in a numerical literal.
+		 * This feature enables you, for example. to separate groups of digits in numeric literals, which can improve the readability
+		 * of your code.
+		 * For instance, if your code contains numbers with many digits, you can use an underscore character to separate digits in 
+		 * groups of three, similar to how you would use a punctuation mark like a comma, or a space, as a separator.*/
+		
+		long creditCardNumber = 1234_5678_9012_3456L;
+		long socialSecurityNumber = 999_99_9999L;
+		float pi =  3.14_15F;
+		long hexBytes = 0xFF_EC_DE_5E;
+		long hexWords = 0xCAFE_BABE;
+		long maxLong = 0x7fff_ffff_ffff_ffffL;
+		byte nybbles = 0b0010_0101;
+		long bytes = 0b11010010_01101001_10010100_10010010;
+		
+		System.out.println("\n1.2.4 Example of Using Underscore Characters in Numeric Literals:");
+		System.out.println("1. long creditCardNumber = 1234_5678_9012_3456L: " + creditCardNumber);
+		System.out.println("2. socialSecurityNumber = 999_99_9999L: " + socialSecurityNumber);
+		System.out.println("3. float pi =  3.14_15F: " + pi);
+		System.out.println("4. long hexBytes = 0xFF_EC_DE_5E: " + hexBytes);
+		System.out.println("5. long hexWords = 0xCAFE_BABE: " + hexWords);
+		System.out.println("6. long maxLong = 0x7fff_ffff_ffff_ffffL: " + maxLong);
+		System.out.println("7. byte nybbles = 0b0010_0101: " + nybbles);
+		System.out.println("8. bytes = 0b11010010_01101001_10010100_10010010: " + bytes);
+		
+		/* 1.2.5 Invalid underscores
+		 * You can place underscores only between digits; you cannot place underscores in the following places:
+		 * - At the beginning or end of a number
+		 * - Adjacent to a decimal point in a floating point literal
+		 * - Prior to an F or L suffix
+		 * - In positions where a string of digits is expected
+		 * The following examples demonstrate valid and invalid underscore placements (which are highlighted) in numeric literals: */
+
+		System.out.println("\n1.2.5 Example of Invalid Uses of Underscore Characters in Numeric Literals:");
+		// Invalid: cannot put underscores adjacent to a decimal point
+		// float pi1 = 3_.1415F;  /// Try to uncomment this line to see the invalid use of underscore
+		System.out.println("1. Cannot put underscores adjacent to a decimal point\n\tfloat pi1 = 3_.1415F;");
+		
+		// Invalid: cannot put underscores adjacent to a decimal point
+		// float pi2 = 3._1415F;  /// Try to uncomment this line to see the invalid use of underscore
+		System.out.println("2. Cannot put underscores adjacent to a decimal point\n\tfloat pi2 = 3._1415F;");
+
+		// Invalid: cannot put underscores prior to an L suffix
+		//long socialSecurityNumber1 = 999_99_9999_L;  /// Try to uncomment this line to see the invalid use of underscore
+		System.out.println("3. Cannot put underscores prior to an L suffix\n\tlong socialSecurityNumber1 = 999_99_9999_L;");
+		
+		// OK (decimal literal)
+		int x1 = 5_2;
+		// Invalid: cannot put underscores at the end of a literal
+		// int x2 = 52_;   /// Try to uncomment this line to see the invalid use of underscore
+		System.out.println("4. Cannot put underscores at the end of a literal\n\tint x2 = 52_;");
+		// OK (decimal literal)
+		int x3 = 5_______2;
+		System.out.println("  *These are okay:");
+		System.out.println("\tint x1 = 5_2; => " + x1);
+		System.out.println("\tint x3 = 5_______2 => " + x3);;
+		// Invalid: cannot put underscores in the 0x radix prefix
+		// int x4 = 0_x52; /// Try to uncomment this line to see the invalid use of underscore
+		System.out.println("5. Cannot put underscores in the 0x radix prefix\n\tint x4 = 0_x52;");
+		
+		// Invalid: cannot put underscores at the beginning of a number
+		// int x5 = 0x_52; /// Try to uncomment this line to see the invalid use of underscore
+		System.out.println("6. Cannot put underscores at the beginning of a number\n\tint x5 = 0x_52;");
+		
+		// OK (hexadecimal literal)
+		int x6 = 0x5_2;
+		
+		// Invalid: cannot put underscores at the end of a number
+		//int x7 = 0x52_; /// Try to uncomment this line to see the invalid use of underscore
+		System.out.println("7. Cannot put underscores at the end of a number\n\tint x7 = 0x52_;");
+		System.out.println("  *This is okay:");
+		System.out.println("\tint x6 = 0x5_2; => " + x6);
+	}
+	
 }
